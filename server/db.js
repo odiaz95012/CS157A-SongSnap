60d59f65d7f579c156a1060b01df4b353c2e380e
Constantine Khamis <constantine@constantinekhamis.com>
2023-11-01 21:21:12 -0700

    Fully able to be created in docker containers

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'Oskie83195012))$',
-        database: 'songsnapdb'
-    }
-)
+const connection = mysql.createConnection({
+    host: 'db', // Using the service name from the docker-compose file
+    user: 'root',
+    password: '1234',
+    database: 'songsnapdb',
+    port: 3306, // The internal MySQL port within the container
+});
 
 module.exports = connection;
