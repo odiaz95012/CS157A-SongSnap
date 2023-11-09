11d466bb1b11208ea39601ab2dea3ea716b51c73
Constantine Khamis <constantine@constantinekhamis.com>
2023-11-08 22:13:53 -0800

    Settings > Friends can now display relevant information. Still working on a bug where database is unchanged even after queries

--- songsnap/src/views/Settings.tsx
+++ songsnap/src/views/Settings.tsx
@@ -6,54 +6,149 @@ import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import axios from 'axios';
+import { PersonCheckFill, PersonFillDash} from 'react-bootstrap-icons';
+
+interface Friend {
+    User1ID: number;
+    Date: string;
+    name: string;
+    username: string;
+}
+
+interface User {
+    ID: number;
+    Username: string;
+    Email: string;
+    Password: string;
+    Role: string;
+    ProfilePicture: string;
+    name: string;
+}
 
 function Settings() {
-    const [userData, setUserData] = useState(null);
-    const [friendRequests, setFriendRequests] = useState(null);
-    const [userFriends, setUserFriends] = useState(null);
+    const [userData, setUserData] = useState<User | null>(null);
+    const [friendRequests, setFriendRequests] = useState<Friend[]>([]);
+    const [userFriends, setUserFriends] = useState<Friend[]>([]);
+
+    const renderFriendRequests = () => {
+        return friendRequests.map((request, index) => (
+            <div key={index} className="row bg-light rounded-2 py-2 mb-3">
+                <div className="col-8">
+                    <a href={'user/' + request.User1ID}><h3 className='fw-bold text-black fw-bold mb-0'>{request.name}</h3></a>
+                </div>
+                <div className="col-2">
+                    <button type="button" className="btn btn-primary btn-sm me-2" onClick={() => respondFriendRequest(request.User1ID, "Accepted")}><PersonCheckFill className='icon' /></button>
+                </div>
+                <div className="col-2">
+                    <button type="button" className="btn btn-danger btn-sm me-2" onClick={() => respondFriendRequest(request.User1ID, "Rejected")}><PersonFillDash className='icon' /></button>
+                </div>
+            </div>
+        ));
+    };
+
+    const renderFriends = () => {
+        return userFriends.map((request, index) => (
+            <div key={index} className="row bg-light rounded-2 py-2 mb-3">
+                <div className="col-10">
+                    <a href={'user/' + request.User1ID}><h3 className='fw-bold text-black fw-bold mb-0'>{request.name}</h3></a>
+                </div>
+                <div className="col-2">
+                    <button type="button" className="btn btn-danger btn-sm me-2" onClick={() => respondFriendRequest(request.User1ID, "Rejected")}><PersonFillDash className='icon' /></button>
+                </div>
+            </div>
+        ));
+    };
+
+    const respondFriendRequest = (ID: number, accept: string) => {
+        if (userData) {
+            const user1ID = userData.ID;
+
+            const requestData = {
+                user1id: user1ID,
+                user2id: ID,
+                decision: accept
+            };
+
+            axios.post('users/friend-requests/respond', requestData)
+                .then(response => {
+                    console.log("Response submitted");
+                    // You might want to update the state or do something else upon success
+                })
+                .catch(error => {
+                    console.error("Error responding to friend request:", error);
+                    // Handle errors accordingly
+                })
+                .finally(() => {
+                    fetchUserData();
+                });
+        } else {
+            console.error('userData is null');
+        }
+    };
+
+    const unAddFriend = (ID:number) => {
+        if (userData) {
+            const user1ID = userData.ID;
+
+            const requestData = {
+                user1id: user1ID,
+                user2id: ID,
+            };
+
+            axios.post('users/friends/remove', requestData)
+                .then(response => {
+                    console.log("Response submitted");
+                    // You might want to update the state or do something else upon success
+                })
+                .catch(error => {
+                    console.error("Error responding to friend request:", error);
+                    // Handle errors accordingly
+                })
+                .finally(() => {
+                    fetchUserData();
+                });
+        } else {
+            console.error('userData is null');
+        }
+    };
 
     const getCookie = (name: string) => {
         return Cookies.get(name);
     }
 
+    const fetchUserData = async () => {
+        // get user id from cookie
+        const userID = await getCookie('userID');
+        // Fetch user data
+        axios.get('/users/id?id=' + userID)
+            .then(response => {
+                setUserData(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching user data:", error);
+            });
+
+        // Fetch friend requests
+        axios.get('/users/friend-requests/all?id=' + userID)
+            .then(response => {
+                setFriendRequests(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching friend requests:", error);
+            });
+
+        // Fetch user's friends
+        axios.get('/users/friends/all?id=' + userID)
+            .then(response => {
+                setUserFriends(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching user's friends:", error);
+            });
+    }
     //const userID = 1;
     useEffect(() => {
-
-        const fetchUserData = async () => {
-            // get user id from cookie
-            const userID = await getCookie('userID');
-            // Fetch user data
-            axios.get('/users/id?id=' + userID)
-                .then(response => {
-                    console.log(response.data)
-                    setUserData(response.data); // Assuming the data retrieved is directly stored in response.data
-                })
-                .catch(error => {
-                    console.error("Error fetching user data:", error);
-                });
-
-            // Fetch friend requests
-            axios.get('/users/friend-requests/all?id=' + userID)
-                .then(response => {
-                    console.log(response.data)
-                    setFriendRequests(response.data); // Assuming the data retrieved is directly stored in response.data
-                })
-                .catch(error => {
-                    console.error("Error fetching friend requests:", error);
-                });
-
-            // Fetch user's friends
-            axios.get('/users/friends/all?id=' + userID)
-                .then(response => {
-                    console.log(response.data)
-                    setUserFriends(response.data); // Assuming the data retrieved is directly stored in response.data
-                })
-                .catch(error => {
-                    console.error("Error fetching user's friends:", error);
-                });
-        }
         fetchUserData();
-
     }, []);
 
     const profileImage = {
@@ -121,20 +216,22 @@ function Settings() {
                                     <h1 className="fw-bold">Friend Management</h1>
                                 </Col>
                                 <Col xs="12" lg="3">
-                                    <h2 className="fw-bold fs-1 text-secondary text-center">3 Requests</h2>
+                                    <h2 className="fw-bold fs-1 text-secondary text-center">{friendRequests ? friendRequests.length : 0} Requests</h2>
                                 </Col>
                                 <Col xs="12" lg="3">
-                                    <h2 className="fw-bold fs-1 text-secondary text-center">6 Friends</h2>
+                                    <h2 className="fw-bold fs-1 text-secondary text-center">{userFriends ? userFriends.length : 0} Friends</h2>
                                 </Col>
                             </Row>
                         </Container>
                         <Container className="mt-4">
                             <Row>
-                                <Col xs={12} lg={6}>
+                                <Col xs={12} lg={6} className='px-3'>
                                     <h4 className="fw-bold text-center">Requests</h4>
+                                    {renderFriendRequests()}
                                 </Col>
-                                <Col xs={12} lg={6}>
+                                <Col xs={12} lg={6} className='px-3'>
                                     <h4 className="fw-bold text-center">Friends</h4>
+                                    {renderFriends()}
                                 </Col>
                             </Row>
                         </Container>
