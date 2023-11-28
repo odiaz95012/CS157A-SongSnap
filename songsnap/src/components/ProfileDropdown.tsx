c6b44557c4093e9099d2a9027facf6b9178ab461
odiaz95012 <odiaz95012@gmail.com>
2023-11-28 15:16:30 -0800

    Added the logging out feature.

--- songsnap/src/components/ProfileDropdown.tsx
+++ songsnap/src/components/ProfileDropdown.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
+import LogOutModal from './LogOutModal';
 
 function ProfileDropdown({ id }: { id: string }) {
   return (
@@ -10,7 +11,9 @@ function ProfileDropdown({ id }: { id: string }) {
       <Dropdown.Menu>
         <Dropdown.Item href={"/profile/" + id}>View Profile</Dropdown.Item>
         <Dropdown.Item href="/settings">Settings</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Log Out</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">
+          <LogOutModal />
+        </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
